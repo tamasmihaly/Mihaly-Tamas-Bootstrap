77fe9983fa6c65341801f42c0e2f64b5035983b6
Mihaly Tamas <mihaly.tamas92@gmail.com>
2018-03-14 16:33:24 +0100

    commit

---
+++ js/myJs.js
@@ -0,0 +1,9 @@
+$("footer .modal-privacy").click(function () {
+    $("footer .modal-privacy").attr("data-target", "#modal-privacy");
+});
+$("footer .modal-terms").click(function () {
+    $("footer .modal-terms").attr("data-target", "#modal-terms");
+});
+$("footer .modal-faq").click(function () {
+    $("footer .modal-faq").attr("data-target", "#modal-faq");
+});
